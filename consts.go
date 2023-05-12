4c3297bfb3baa4e6fa185520c3d632798fde5983
mark-summerfield <mark@qtrac.eu>
2023-05-12 12:50:35 +0100

    dropped bolt k-v and now trying sqlite3

--- consts.go
+++ consts.go
@@ -9,22 +9,6 @@ var (
 	//go:embed Version.dat
 	Version string
 
-	fileFormat byte = 1
-
-	configBucket  = []byte("config")
-	statesBucket  = []byte("states")
-	savesBucket   = []byte("saves")
-	renamedBucket = []byte("renamed")
-
-	configFormat = []byte("format")
-	configIgnore = []byte("ignore")
-
-	saveWhen           = []byte("*when")
-	saveComment        = []byte("*comment")
-	savePredefinedKeys = 2 // how many of the above
-
-	emptyValue = []byte{}
-
 	// Should also ignore hidden (.) files and subdirs by default.
 	defaultIgnores = []string{"*#[0-9].*", "*.a", "*.bak", "*.class",
 		"*.dll", "*.exe", "*.fhd", "*.jar", "*.ld", "*.ldx", "*.li",
@@ -32,6 +16,58 @@ var (
 		"*.swp", "*.tmp", "*~", "gpl-[0-9].[0-9].txt", "louti[0-9]*",
 		"moc_*.cpp", "qrc_*.cpp", "ui_*.h"}
 
-	uint16size = 2
-	uint32size = 4
+	dbDriverName    = "sqlite3"
+	dbFormatVersion = 1
+
+	dbPragmas = []string{
+		`PRAGMA encoding = 'UTF-8';`,
+		`PRAGMA foreign_keys = TRUE;`,
+		`PRAGMA synchronous = NORMAL;`,
+		`PRAGMA temp_store = MEMORY;`}
+
+	dbTables = []string{
+		`CREATE TABLE names (
+			fid INTEGER NOT NULL,
+			sid INTEGER NOT NULL,
+			filename TEXT NOT NULL,
+
+			PRIMARY KEY(fid, sid),
+			FOREIGN KEY(fid) REFERENCES states(fid),
+			FOREIGN KEY(sid) REFERENCES saves(sid),
+			CHECK(fid > 0),
+			CHECK(sid > 0),
+			CHECK(LENGTH(filename) > 0)
+		)`,
+		`CREATE TABLE saves (
+			sid INTEGER NOT NULL PRIMARY KEY,
+			fid INTEGER NOT NULL,
+			sha TEXT NOT NULL,
+			flag TEXT NOT NULL,
+			data BLOB,
+
+			FOREIGN KEY(fid) REFERENCES states(fid),
+			CHECK(fid > 0),
+			CHECK(sid > 0),
+			CHECK(flag IN ('R', 'F', 'L'))
+		)`,
+		`CREATE TABLE saveinfo (
+			sid INTEGER NOT NULL PRIMARY KEY,
+			timestamp DATETIME NOT NULL,
+			comment TEXT,
+
+			FOREIGN KEY(sid) REFERENCES saves(sid),
+			CHECK(sid > 0)
+		)`,
+		`CREATE TABLE states (
+			fid INTEGER NOT NULL PRIMARY KEY,
+			monitored TEXT NOT NULL,
+			lastsid INTEGER NOT NULL,
+			lastkind TEXT NOT NULL,
+
+			CHECK(fid > 0),
+			CHECK(monitored IN ('Y', 'N')),
+			CHECK(lastsid > 0),
+			CHECK(lastkind IN ('B', 'I', 'T'))
+		)`,
+	}
 )
--- go.mod
+++ go.mod
@@ -2,11 +2,11 @@ module github.com/mark-summerfield/fhd
 
 go 1.20
 
-require go.etcd.io/bbolt v1.3.7
-
 require (
 	github.com/mark-summerfield/gong v1.4.2
-	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53
+	github.com/mattn/go-sqlite3 v1.14.16
+	go.etcd.io/bbolt v1.3.7
+	golang.org/x/exp v0.0.0-20230510235704-dd950f8aeaea
 )
 
 require golang.org/x/sys v0.8.0 // indirect
