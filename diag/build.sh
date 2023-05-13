7d8cbf8cb1c54a61794ca2e70fa5443c185c87ef
mark-summerfield <mark@qtrac.eu>
2023-05-13 09:27:15 +0100

    better diag

--- diag/build.sh
+++ diag/build.sh
@@ -1,5 +1,5 @@
 lout -U -C/home/mark/fonts -D/home/mark/fonts -F/home/mark/fonts \
     -o db.ps main.lout && \
     embedfonts.py db.ps > /dev/null && \
-    pspdf -W 288 -H 470 db.ps db.pdf > /dev/null && \
+    pspdf -W 324 -H 425 db.ps db.pdf > /dev/null && \
     pdf2svg db.pdf db.svg && rm -f db.ps db.pdf
